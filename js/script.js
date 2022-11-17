525c5365cb082d3d6cefd83323a70b86b560158a
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2022-11-17 07:19:54 -0300

    comecando aula 1 javascript e video funcionando

--- js/script.js
+++ js/script.js
@@ -3,5 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+    // codigo para aula_1_criptomoeda.html
+    e = document.querySelector(".submit");
+    e.addEventListener("click", function() {
+        resposta = document.querySelector('input[name="pergunta1"]:checked').value;
+        console.log(resposta);
+        if (resposta == "Bitcoin") {
+            console.log("OK");
+        } else if (resposta == "") {
+            alert("BRUH")
+        }
+    });
+
     
     })
