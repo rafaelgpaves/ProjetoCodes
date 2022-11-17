02e6f544dad19c8d17bfe3defd8a15dd7057c7c4
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2022-11-17 07:47:27 -0300

    js aula 1 ainda não terminado

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-    // codigo para aula_1_criptomoeda.html
+    // codigo para aula_1_criptomoeda.html:
     e = document.querySelector(".submit");
     e.addEventListener("click", function() {
-        resposta = document.querySelector('input[name="pergunta1"]:checked').value;
-        console.log(resposta);
-        if (resposta == "Bitcoin") {
-            console.log("OK");
-        } else if (resposta == "") {
+        try {
+            resposta = document.querySelector('input[name="pergunta1"]:checked').value;
+            console.log(resposta);
+            if (resposta == "Bitcoin") {
+                console.log("OK");
+            } else {
+                console.log("Errou")
+            }
+        }
+        // se nãoo der certo o bloco try, vai entrar no catch, se a pessoa não tiver escolhido nenhuma opção
+        catch {
             alert("BRUH")
         }
+                 
     });
 
-    
     })
