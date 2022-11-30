da49b350e07e2a9dda755ee59a6e4d9ddfd2b1ac
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2022-11-30 14:27:51 -0300

    aula 3 e consertando aula 2

--- js/aula_3_cripto.js
+++ js/aula_3_cripto.js
@@ -7,40 +7,40 @@ document.addEventListener('DOMContentLoaded', function() {
     e = document.querySelector(".submit");
     e.addEventListener("click", function() {
         try {
-            p = document.getElementById("feedback2");
-            resposta = document.querySelector('input[name="pergunta2"]:checked').value;
+            p = document.getElementById("feedback3");
+            resposta = document.querySelector('input[name="pergunta3"]:checked').value;
             console.log(resposta);
-            if (resposta == "dezmil") {
-                localStorage.setItem("pergunta2", "acertou");
-                p.innerHTML = "Parabéns, você já acertou a pergunta! A resposta certa era 10 mil";
+            if (resposta == "sana") {
+                localStorage.setItem("pergunta3", "acertou");
+                p.innerHTML = "Parabéns, você já acertou a pergunta! A resposta certa era Satoshi Nakamoto";
                 e.remove();
                 resposta_certa = document.getElementById("resposta");
                 resposta_certa.style.border = "0.1rem solid #3a5d69";
 
-                a.href = "aula_3_criptomoeda.html";
+                a.href = "aula_4_criptomoeda.html";
                 a.style.color = "#19c6ff";
                 a.innerHTML = "Clique aqui para ir para a próxima aula";
             } else {
-                localStorage.setItem("pergunta2", "errou");
+                localStorage.setItem("pergunta3", "errou");
                 p.innerHTML = "Essa não é a resposta certa. Reveja o vídeo e tente novamente";
             }
         }
         // se não der certo o bloco try, vai entrar no catch, se a pessoa não tiver escolhido nenhuma opção
         catch {
-            p = document.getElementById("feedback2");
+            p = document.getElementById("feedback3");
             p.innerHTML = "Escolha alguma resposta!";
         }
 
     });
 
-    if (localStorage.getItem("pergunta2") == "acertou") {
-        p = document.getElementById("feedback2");
-        p.innerHTML = "Parabéns, você já acertou a pergunta! A resposta certa era 10 mil";
+    if (localStorage.getItem("pergunta3") == "acertou") {
+        p = document.getElementById("feedback3");
+        p.innerHTML = "Parabéns, você já acertou a pergunta! A resposta certa era Satoshi Nakamoto";
         e.remove();
         resposta_certa = document.getElementById("resposta");
         resposta_certa.style.border = "0.1rem solid #3a5d69";
 
-        a.href = "aula_3_criptomoeda.html";
+        a.href = "aula_4_criptomoeda.html";
         a.style.color = "#19c6ff";
         a.innerHTML = "Clique aqui para ir para a próxima aula";
     }
