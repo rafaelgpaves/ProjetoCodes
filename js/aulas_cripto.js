2a8fa62081f62867bf89245b555a9f0be961292e
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2022-12-05 12:32:09 -0300

    certinho em aulas completas

--- js/aulas_cripto.js
+++ js/aulas_cripto.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    aula1 = document.getElementById("aula1");
     aula2 = document.getElementById("aula2");
     aula3 = document.getElementById("aula3");
     aula4 = document.getElementById("aula4");
     if (localStorage.getItem("pergunta1") == "acertou") {
         aula2.href = "aula_2_criptomoeda.html";
+        aula1.innerHTML = "Aula 1 - ✓✓"
     }
     if (localStorage.getItem("pergunta2") == "acertou") {
         aula3.href = "aula_3_criptomoeda.html";
+        aula2.innerHTML = "Aula 2 - ✓✓"
     }
     if (localStorage.getItem("pergunta3") == "acertou") {
         aula4.href = "aula_4_criptomoeda.html";
+        aula3.innerHTML = "Aula 3 - ✓✓"
+    }
+    if (localStorage.getItem("pergunta4") == "acertou") {
+        aula4.innerHTML = "Aula 4 - ✓✓"
     }
 
     aula2.addEventListener("click", function() {
